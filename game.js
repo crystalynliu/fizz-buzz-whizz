a45640e845d6bba674dc498ece597641b2f65d5e
crystalynliu <crystalynliu@163.com>
2016-02-23 12:01:50 +0800

    modify the game.js that add the function ruleOne

--- game.js
+++ game.js
@@ -1,28 +1,38 @@
 var specNum = [3,5,7];
+var wordArr = ["Fizz","Buzz","Whizz"];
 
 function calculate (num) {
 	var result = "";
-	if(num.toString().indexOf(specNum[0].toString())!=-1){
-		result = "Fizz";
-	}
-	else if(num%(specNum[0]*specNum[1]*specNum[2])==0){
-		result="FizzBuzzWhizz";
-	}
-	else if(num%(specNum[0]*specNum[1])==0){
-		result="FizzBuzz";
-	}else if(num%(specNum[1]*specNum[2])==0){
-		result="BuzzWhizz";
-	}else if(num%(specNum[0]*specNum[2])==0){
-		result="FizzWhizz";
-	}
-	else if(num%specNum[0]==0){
-		result="Fizz";
-	}else if(num%specNum[1]==0){
-		result="Buzz";
-	}else if(num%specNum[2]==0){
-		result="Whizz";
+	if(ruleOne(num)==false){
+		if(num%(specNum[0]*specNum[1]*specNum[2])==0){
+			result="FizzBuzzWhizz";
+		}
+		else if(num%(specNum[0]*specNum[1])==0){
+			result="FizzBuzz";
+		}else if(num%(specNum[1]*specNum[2])==0){
+			result="BuzzWhizz";
+		}else if(num%(specNum[0]*specNum[2])==0){
+			result="FizzWhizz";
+		}
+		else if(num%specNum[0]==0){
+			result="Fizz";
+		}else if(num%specNum[1]==0){
+			result="Buzz";
+		}else if(num%specNum[2]==0){
+			result="Whizz";
+		}else{
+			result=num;
+		}
 	}else{
-		result=num;
-	}
+		result=ruleOne(num);
+	};
 	return result;
 }
+
+function ruleOne(num){
+	result=num.toString().indexOf(specNum[0].toString())!=-1?wordArr[0]:false;
+	return result;
+}
+
+
+
